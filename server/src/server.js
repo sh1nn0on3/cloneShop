128ceec01b00903f22cf7a04cf8b71e52c42e9e3
sh1nn0on3 <sanogaku31@gmail.com>
2023-05-04 18:09:21 +0700

    add

--- server/src/server.js
+++ server/src/server.js
@@ -3,19 +3,19 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
-// import cors from "cors";
+import cors from "cors";
 import upData from "../data/upData"
 
 
 require("dotenv").config();
 let app = express();
 app.use(bodyParser.json());
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 viewEngine(app);
 initWebRoutes(app);
 connectDB();
-// upData()
+upData()
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
   console.log("Backend Nodejs is running on the port : " + port);
